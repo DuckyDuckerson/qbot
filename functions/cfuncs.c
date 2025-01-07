3168608b364d2ecd5fa59f1ae17e740319710516
Ducky Duckerson <102842567+DuckyDuckerson@users.noreply.github.com>
2025-01-07 03:33:04 -0700

    creating http server

--- functions/cfuncs.c
+++ functions/cfuncs.c
@@ -4,16 +4,19 @@
 #include <string.h>
 
 
-int message_length(char *message) {
+int message_length(char *message) 
+{
   int i = 0;
-  while (message[i] != '\0') {
+  while (message[i] != '\0') 
+  {
     i++;
   }
   return i;
 }
 
 
-int xp_alg(char *message) {
+int xp_alg(char *message) 
+{
   double xp;
   double modifer = 1.5;
   int ppm = 100;
@@ -24,8 +27,10 @@ int xp_alg(char *message) {
 }
 
 
-void printing(int index, const char *message, float delay) {
-  if (index < strlen(message)) {
+void printing(int index, const char *message, float delay) 
+{
+  if (index < strlen(message)) 
+  {
     putchar(message[index]);
     fflush(stdout);
     usleep((int)(delay * 1000000)); // convert delay to microseconds
@@ -34,19 +39,22 @@ void printing(int index, const char *message, float delay) {
 }
 
 
-void print_message(const char *message, int speed, int newline) {
+void print_message(const char *message, int speed, int newline) 
+{
   float speed_map[6] = {0, 0.2, 0.05, 0.01, 0.007, 0.003};
   float delay = speed_map[speed];
    
   printing(0, message, delay);
     
-  if (newline == 1) {
+  if (newline == 1) 
+  {
     putchar('\n');
   }
 }
 
 
-int shell_commands() {
+int shell_commands() 
+{
   system("ls -l");
   system("pwd");
 }
