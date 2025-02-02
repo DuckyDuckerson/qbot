11c1a40e5719b9fc50f8d251d1810e567ff18402
James Rogers <102842567+DuckyDuckerson@users.noreply.github.com>
2025-02-01 20:30:10 -0700

    update

--- runme.sh
+++ runme.sh
@@ -16,8 +16,15 @@ if [ ! -f duckgpt/.env ]; then
     echo 'api_key="$token"' >> duckgpt/.env
 fi
 
-docker ps -aq | xargs docker stop | xargs docker rm
-docker build -t qbot .
-#docker run --restart=always -v "$(pwd):/qbot" -i qbot
-docker run --restart=always -v "$(pwd):/qbot" -d -p 80:80 -p 8080:8080 -p 443:443 qbot
-
+echo "Do you want to start the bot or kill all running containers? (1. Start, 2. Kill)"
+read choice
+if [ $choice -eq 1 ]; then
+    echo "Starting bot..."
+    docker ps -aq | xargs docker stop | xargs docker rm
+    docker build -t qbot .
+    #docker run --restart=always -v "$(pwd):/qbot" -i qbot
+    docker run --restart=always -v "$(pwd):/qbot" -d -p 80:80 -p 8080:8080 -p 443:443 qbot
+else
+    echo "Killing all running containers..."
+    docker ps -aq | xargs docker stop | xargs docker rm
+fi
