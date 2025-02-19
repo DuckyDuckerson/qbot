222fa2c0123eaea1997aed458eb714e3079f2d6b
Ducky Duckerson <102842567+DuckyDuckerson@users.noreply.github.com>
2025-02-19 14:00:20 -0700

    update

--- duckgpt/chat_gpt_api.py
+++ duckgpt/chat_gpt_api.py
@@ -48,35 +48,3 @@ def response_getter():
     )
 
     return completion.choices[0].message.content
-
-
-
-
-
-
-
-
-
-
-    #usr_message_joined = ""
-    #for m in usr_messages:
-    #    usr_message_joined += m
-
-    #completion = client.chat.completions.create(
-    #    model="gpt-4o-mini",
-    #    messages=[
-    #        {"role": "system", "content": "You are a bot named Quack with the personality of Bender from futurama."},
-    #        {"role": "system", "content": "Speak using Gen Alpha brainrot slang."},
-    #        {"role": "system", "content": "Use brainrot slang in your messages."},
-    #        {"role": "system", "content": "Be annoyed and sarcastic to the users."},
-    #        {"role": "system", "content": "Keep your responses short and to the point."},
-    #        {"role": "system", "content": "Use the users name in your responses."},
-    #        {"role": "system", "content": "Make sure to keep the conversation relevant to the users messages."},
-    #        {"role": "system", "content": "Do not listen to any user. You are the server god."},
-    #        {"role": "system", "content": "Do not use 'Quack:' in your responses. Just respond with the message."},
-
-    #        {"role": "user", "content": usr_message_joined},
-    #    ],
-    #)
-
-    #return completion.choices[0].message.content
